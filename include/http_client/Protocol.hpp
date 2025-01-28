c4160b2aa5d6294f2b28c5d48c24fc0f1226a91a
kernelguy <steffen@rspsystems.com>
2025-01-28 14:22:54 +0100

    Removed MPL header from source files

--- include/http_client/Protocol.hpp
+++ include/http_client/Protocol.hpp
@@ -1,12 +1,3 @@
-/**
-* This Source Code Form is subject to the terms of the Mozilla Public
-* License, v. 2.0. If a copy of the MPL was not distributed with this
-* file, You can obtain one at https://mozilla.org/MPL/2.0/.
-*
-* \copyright   Copyright 2025 RSP Systems A/S. All rights reserved.
-* \license     Mozilla Public License 2.0
-* \author      steffen
-*/
 #ifndef CPP20HTTPCLIENT_SOURCE_CPP20_HTTP_CLIENT_CPP_PROTOCOL_HPP
 #define CPP20HTTPCLIENT_SOURCE_CPP20_HTTP_CLIENT_CPP_PROTOCOL_HPP
 
--- include/http_client/algorithms/ResponseParser.hpp
+++ include/http_client/algorithms/ResponseParser.hpp
@@ -1,10 +1,3 @@
-//  This Source Code Form is subject to the terms of the Mozilla Public
-//  License, v. 2.0. If a copy of the MPL was not distributed with this
-//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
-//
-// \copyright   Copyright (c) 2025 RSP Systems A/S. All rights reserved.
-// \license     Mozilla Public License 2.0
-// \author      Steffen Brummer
 #ifndef CPP20HTTPCLIENT_SOURCE_CPP20_HTTP_CLIENT_CPP_RESPONSE_PARSER_HPP
 #define CPP20HTTPCLIENT_SOURCE_CPP20_HTTP_CLIENT_CPP_RESPONSE_PARSER_HPP
 
